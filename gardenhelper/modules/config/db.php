548ab7ac5675d1691f0259575471afa41055192e
Yurii Lisovskyi <yuri.lisovski@gmail.com>
2022-04-25 15:53:13 +0300

    renew1

---
+++ gardenhelper/modules/config/db.php
@@ -0,0 +1,1 @@
+<?php$conn = new mysqli($server, $username, $password, $dbname);mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);if($conn->connect_error) {	die("Connection failed: " . $connect->connect_error);}// $conn->set_charset("utf8");//mysqli_set_charset - функция для корректного отображения руского языкаmysqli_set_charset($conn, "utf8");?>
---
+++ gardenhelper/modules/user/islogin.php
@@ -0,0 +1,1 @@
+<?php $data = [	'status' => false,];if (isset($_COOKIE['user_id'])) {	$data = [		'status' => true,		'id' => $_COOKIE['user_id']	];}echo json_encode($data);?>
